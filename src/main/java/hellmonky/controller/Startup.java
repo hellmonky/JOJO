30a9dc427a93072b9cfc9071d3926d5729b948a1
HZ\yuanlai.xwt <yuanlai.xwt@alibaba-inc.com>
2017-06-29 13:56:20 +0800

    tag test

--- src/main/java/hellmonky/controller/Startup.java
+++ src/main/java/hellmonky/controller/Startup.java
@@ -16,24 +16,14 @@ import sun.rmi.runtime.Log;
 
 public class Startup extends Application {
 
-    /*
-    @Context
-    private Configuration configuration;
-    */
-
     @Override
     public Set<Class<?>> getClasses() {
-        /* 检查当前环境
-        Collection<String> names = configuration.getPropertyNames();
-        for(String name : names){
-            System.out.println(name);
-        }
-        //*/
-
         LogUtil.getInstance().error("startup process.......");
         //System.out.println("startup process.......");
 
-        // 添加特性
+        /**
+         * 添加启动时需要的特性
+         */
         final Set<Class<?>> classes = new HashSet<Class<?>>();
         // register resources and features
         classes.add(LogUtil.class);
